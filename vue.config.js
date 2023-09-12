16ad33a0158ee0906cd2a7c8d7052f2fa662fa04
bowen99991 <956125910@qq.com>
2023-09-12 16:53:08 +0800

    更改config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/smartMall_m/' // 你的GitHub仓库名称
-    : '/'
+    : '/',
   // 其他配置选项...
+  outputDir: 'dist',
+  assetsDir: 'static'
 })
