334b9708afc63937e24b94a226583d3cfa78e7dd
bowen99991 <956125910@qq.com>
2023-09-12 12:54:50 +0800

    router

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/smartMall_m/dist' // 你的GitHub仓库名称
+    ? '/smartMall_m/' // 你的GitHub仓库名称
     : '/'
   // 其他配置选项...
 })
