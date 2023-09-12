3d3e6fb22b4dac1e31e8465c19912d8c8c694ab4
麦田守望者 <129667069+bowen99991@users.noreply.github.com>
2023-09-12 17:12:20 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+ transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/smartMall_m/' // 你的GitHub仓库名称
+    : '/',
+  // 其他配置选项...
+  outputDir: 'dist',
+  assetsDir: 'static'
 })
